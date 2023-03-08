0e5958930df2669e0f627026be1a0d1d618a79b9
lando <landopl95@gmail.com>
2023-03-08 14:35:44 -0500

    Primer commit-contadorDeClics

---
+++ contador-de-clics/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import freeCodeCamp from './imagenes/freecodecamp-logo.png'
+import Boton from './componentes/Boton.js'
+import Contador from './componentes/Contador.js'
+import { useState } from 'react';
+
+function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0)
+  }
+
+  return (
+    <div className="App">
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          className='freecodecamp-logo'
+          src= {freeCodeCamp}
+          alt= 'logo de freCodeCamp' />
+      </div>
+
+      <div className='contenedor-principal'>
+        <Contador
+          numClics = {numClics}
+        />
+        <Boton
+          texto= 'Clic'
+          esBotonDeClic={true}
+          manejarClic ={manejarClic}
+        />   
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic= {reiniciarContador}
+        />
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
